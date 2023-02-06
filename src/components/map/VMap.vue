bc1b7d494f56125142547a84892ca89806e0525d
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-02-06 23:15:46 +0300

    feat: add docgen

--- src/components/map/VMap.vue
+++ src/components/map/VMap.vue
@@ -1,3 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+export interface Props {
+  /**
+   * Initial geographic center of the map
+   */
+  center?: number[];
+}
+
+withDefaults(defineProps<Props>(), {
+  center: () => [0, 0]
+});
+
+defineEmits<{
+  /**
+   * Trigger viewport-changed
+   * @property center number[]
+   */
+  (e: 'viewport-changed', center: number[]): void;
+}>();
+
+defineExpose({
+  /**
+   * Leaflet instance
+   */
+  ref: ref<number | null>(null)
+});
+</script>
+
 <template>
-  <div>Map</div>
+  <div>
+    <span>Map: </span>
+    <!--
+      @slot Default slot
+    -->
+    <slot></slot>
+    <!--
+      @slot Map slot
+    -->
+    <slot name="map"></slot>
+  </div>
 </template>
