c6102606e45ccb1ef617a5b4412fc1576035059a
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-03-29 17:59:21 +0300

    fix: playground

--- playground/demo/control/layers-control.vue
+++ playground/demo/control/layers-control.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { VMap, VMapOsmTileLayer, VMapLayersControl } from 'vue-map-ui';
+
+const currentBaseLayer = ref<string | undefined>(undefined);
 </script>
 
 <template>
   <VMap style="height: 200px">
-    <VMapLayersControl>
+    <VMapLayersControl v-model:current-base-layer="currentBaseLayer">
       <VMapOsmTileLayer />
     </VMapLayersControl>
   </VMap>
+  <div>Current Base Layer: {{ currentBaseLayer }}</div>
 </template>
