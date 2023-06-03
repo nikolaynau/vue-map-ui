ecd201234419e0b2003f2adc59dd747fe979b02f
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-06-03 21:19:08 +0300

    fix: types

--- src/components/map/composables/usePane.ts
+++ src/components/map/composables/usePane.ts
@@ -2,7 +2,7 @@ import { inject, provide, readonly, type Ref } from 'vue';
 import { paneKey } from './injectionSymbols';
 
 export function providePane(pane: Ref<HTMLElement | null>) {
-  provide(paneKey, readonly(pane));
+  provide(paneKey, readonly(pane) as Ref<HTMLElement | null>);
 }
 
 export function usePane(): Ref<HTMLElement | null> {
