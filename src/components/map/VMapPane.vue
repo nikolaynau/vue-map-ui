d49e93983d7bde7183d32e33619278c29a97b672
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-06-25 14:27:42 +0300

    feat: add class name to pane

--- src/components/map/VMapPane.vue
+++ src/components/map/VMapPane.vue
@@ -10,24 +10,27 @@ export default defineComponent({
 import { computed, toRefs, watch } from 'vue';
 import { isDefined } from '@vueuse/shared';
 import { useLeafletPane, useLeafletReady } from 'vue-use-leaflet';
-import { providePane, useMap } from '.';
+import { useCssClass } from '../../composables';
+import { providePane, useMap } from './composables';
 
 export interface Props {
   name: string;
   zIndex?: number;
+  class?: any;
 }
 
 export type Attrs = any;
 
 const props = defineProps<Props>();
+const { name, class: _class } = toRefs(props);
 
-const { name } = toRefs(props);
 const map = useMap();
 const { paneElements } = useLeafletPane(map, name, { zIndex: props.zIndex });
 const paneElement = computed<HTMLElement | null>(
   () => paneElements.value[name.value] ?? null
 );
 const ready = useLeafletReady(paneElement);
+useCssClass(paneElement, _class);
 
 watch(
   () => props.zIndex,
--- src/components/map/__tests__/VMapPane.spec.ts
+++ src/components/map/__tests__/VMapPane.spec.ts
@@ -110,4 +110,64 @@ describe('VMapPane', () => {
 
     expect(componentRef.value?.paneElement?.style.zIndex).toBe('auto');
   });
+
+  it('init css class', () => {
+    const componentRef = ref<InstanceType<typeof VMapPane> | null>(null);
+    mount(
+      wrap(
+        map,
+        VMapPane,
+        { name: 'paneA', class: 'classA' },
+        undefined,
+        componentRef
+      )
+    );
+
+    expect(componentRef.value?.paneElement).toBeInstanceOf(HTMLElement);
+    expect(
+      componentRef.value?.paneElement?.classList.contains('classA')
+    ).toBeTruthy();
+  });
+
+  it('change css class', async () => {
+    const props = reactive<{ name: string; class: any }>({
+      name: 'paneA',
+      class: undefined
+    });
+    const componentRef = ref<InstanceType<typeof VMapPane> | null>(null);
+    mount(wrap(map, VMapPane, props, undefined, componentRef));
+
+    expect(componentRef.value?.paneElement).toBeInstanceOf(HTMLElement);
+
+    props.class = 'classA';
+    await nextTick();
+
+    expect(
+      componentRef.value?.paneElement?.classList.contains('classA')
+    ).toBeTruthy();
+
+    props.class = ['classB', 'classC'];
+    await nextTick();
+
+    expect(
+      componentRef.value?.paneElement?.classList.contains('classB')
+    ).toBeTruthy();
+    expect(
+      componentRef.value?.paneElement?.classList.contains('classC')
+    ).toBeTruthy();
+
+    props.class = { classD: true, classE: false };
+    await nextTick();
+
+    expect(
+      componentRef.value?.paneElement?.classList.contains('classD')
+    ).toBeTruthy();
+
+    props.class = undefined;
+    await nextTick();
+
+    expect(componentRef.value?.paneElement?.className).toBe(
+      'leaflet-pane leaflet-paneA-pane'
+    );
+  });
 });
