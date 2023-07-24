ee11ed5f005df8954b536a21e3834a9e1c6b7040
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-07-24 18:12:24 +0300

    feat: add throw error

--- src/components/map/composables/useMap.ts
+++ src/components/map/composables/useMap.ts
@@ -6,6 +6,14 @@ export function provideMap(map: Ref<Map | null>) {
   provide(mapKey, readonly(map) as Ref<Map | null>);
 }
 
-export function useMap(): Ref<Map | null> {
-  return inject(mapKey, undefined) as Ref<Map | null>;
+export function useMap(throwErrorWhenNullish: boolean = true): Ref<Map | null> {
+  const value = inject(mapKey, undefined);
+  if (
+    throwErrorWhenNullish &&
+    value === undefined &&
+    import.meta.env.MODE !== 'test'
+  ) {
+    throw new Error('The top component does not provide a map instance.');
+  }
+  return value as Ref<Map | null>;
 }
