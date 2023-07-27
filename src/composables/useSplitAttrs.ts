fc166717e3dd1a92a38c3937b80695fea25c3be0
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-07-27 19:51:03 +0300

    feat: add split attrs

--- src/composables/useSplitAttrs.ts
+++ src/composables/useSplitAttrs.ts
@@ -1,7 +1,46 @@
+import { camelize, toRaw, useAttrs } from 'vue';
+import { isEmpty, lcFirst } from '../utils/strings';
+
+export interface UseSplitAttrsOptions {
+  camelizeKeys?: boolean;
+  rawValue?: boolean;
+}
+
 export type UseSplitAttrsReturn<T = Record<string, unknown>> = {
   default: T;
 } & Record<string, T>;
 
-export function useSplitAttrs(prefixes: string[]): UseSplitAttrsReturn {
-  return { default: {} };
+export function useSplitAttrs(
+  prefixes: string[],
+  options: UseSplitAttrsOptions = {}
+): UseSplitAttrsReturn {
+  const { camelizeKeys, rawValue } = options;
+  const result: UseSplitAttrsReturn = { default: {} };
+  const attrs = useAttrs();
+  let handled;
+
+  for (const key in attrs) {
+    handled = false;
+    for (let i = 0; i < prefixes.length; i++) {
+      const prefix = prefixes[i];
+      if (!result[prefix]) {
+        result[prefix] = {};
+      }
+      if (key.startsWith(prefix)) {
+        const attrName = lcFirst(key.slice(prefix.length));
+        if (!isEmpty(attrName)) {
+          result[prefix][camelizeKeys ? camelize(attrName) : attrName] =
+            rawValue ? toRaw(attrs[key]) : attrs[key];
+          handled = true;
+        }
+      }
+    }
+    if (!handled) {
+      result.default[camelizeKeys ? camelize(key) : key] = rawValue
+        ? toRaw(attrs[key])
+        : attrs[key];
+    }
+  }
+
+  return result;
 }
