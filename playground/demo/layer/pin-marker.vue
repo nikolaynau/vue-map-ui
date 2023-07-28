5bb0513932a6864bde7888f51ab71c8d4a46cdb9
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-07-28 18:20:28 +0300

    fix(demo): pin marker

--- playground/demo/layer/pin-marker.vue
+++ playground/demo/layer/pin-marker.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
 import { VMap, VMapOsmTileLayer, VMapPinMarker } from 'vue-map-ui';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faArrowDown);
+
+const color = ref('');
+const colors = reactive([
+  '#e74645',
+  '#fb7756',
+  '#facd60',
+  '#fdfa66',
+  '#1ac0c6'
+]);
+const latlng = ref<[number, number]>([-40, 0]);
+
+function onChangePosition() {
+  latlng.value = [-45, -10];
+}
 </script>
 
 <template>
@@ -19,8 +34,20 @@ library.add(faArrowDown);
     >
       <FontAwesomeIcon icon="fas fa-arrow-down" />
     </VMapPinMarker>
-    <VMapPinMarker :latlng="[-40, 0]" icon-color="#1ac0c6">
+    <VMapPinMarker :latlng="latlng" :icon-color="color">
       <i class="fa-solid fa-bolt-lightning"></i>
     </VMapPinMarker>
   </VMap>
+  <div class="p-4">
+    <div @click="onChangePosition">Change Position</div>
+    <div>
+      <label
+        >Color ({{ color || 'None' }}):
+        <select v-model="color">
+          <option value="">None</option>
+          <option v-for="c in colors" :key="c" :value="c">{{ c }}</option>
+        </select>
+      </label>
+    </div>
+  </div>
 </template>
