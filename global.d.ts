b848e957af20c2d63706029209c9c5efc3d3a57c
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-08-02 15:45:18 +0300

    fix: global

--- global.d.ts
+++ global.d.ts
@@ -2,10 +2,18 @@
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     VMapAttributionControl: typeof import('vue-map-ui')['VMapAttributionControl'];
+    VMapControlPosition: typeof import('vue-map-ui')['VMapControlPosition'];
     VMapLayersControl: typeof import('vue-map-ui')['VMapLayersControl'];
     VMapScaleControl: typeof import('vue-map-ui')['VMapScaleControl'];
     VMapZoomControl: typeof import('vue-map-ui')['VMapZoomControl'];
+    VMapDefaultIcon: typeof import('vue-map-ui')['VMapDefaultIcon'];
+    VMapDivIcon: typeof import('vue-map-ui')['VMapDivIcon'];
+    VMapDivMarker: typeof import('vue-map-ui')['VMapDivMarker'];
+    VMapIcon: typeof import('vue-map-ui')['VMapIcon'];
+    VMapIconMarker: typeof import('vue-map-ui')['VMapIconMarker'];
     VMapMarker: typeof import('vue-map-ui')['VMapMarker'];
+    VMapPinIcon: typeof import('vue-map-ui')['VMapPinIcon'];
+    VMapPinMarker: typeof import('vue-map-ui')['VMapPinMarker'];
     VMapTileLayer: typeof import('vue-map-ui')['VMapTileLayer'];
     VMap: typeof import('vue-map-ui')['VMap'];
     VMapPane: typeof import('vue-map-ui')['VMapPane'];
